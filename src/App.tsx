bb88ae573f37690ed6bf1bf3dca59b9195502b46
Jaunty <jaunty5899@gmail.com>
2025-07-07 12:54:52 +0530

    header added

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline"> Hello world! </h1>
+      <h1 className="text-5xl">Hello world!</h1>
     </>
   );
 }
