a22d7095cc3ffe411b419ba04356111b19727752
Jaunty <jaunty5899@gmail.com>
2025-07-30 14:54:06 +0530

    alignment fixes

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,29 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="flex flex-col gap-10 items-center bg-gray-950">
-      <header className="flex justify-center bg-emerald-300 drop-shadow-md w-full">
+    <div className="flex flex-col gap-10 items-center">
+      <header className="flex h-40 justify-center bg-emerald-300 drop-shadow-md w-screen">
         <figure>
-          <img src={logo} className="size-30 select-none" />
+          <img src={logo} className="size-40 select-none" />
         </figure>
       </header>
-      <article className="flex justify-end gap-5">
-        <figure className="size-60 rounded-2xl overflow-hidden">
+      <article className="flex gap-5">
+        <figure className=" size-60 rounded-2xl overflow-hidden">
           <img src={mugShot} className="select-none" />
         </figure>
+        <p className="w-110 text-justify">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+          voluptatibus dolor veritatis ipsa, veniam cumque accusantium
+          dignissimos! Ducimus tempora sed numquam repudiandae dicta, labore
+          vero incidunt accusantium. Fugiat, nobis aperiam? vero incidunt
+          accusantium. Fugiat, nobis aperiam? vero incidunt accusantium. Fugiat,
+          nobis aperiam? vero incidunt accusantium. Fugiat, nobis aperiam? vero
+          incidunt accusantium. Fugiat, nobis aperiam? vero incidunt
+          accusantium. Fugiat, nobis aperiam? vero incidunt accusantium. Fugiat,
+          nobis aperiam?
+        </p>
       </article>
+      <article className="flex gap-5"></article>
     </div>
   );
 }
